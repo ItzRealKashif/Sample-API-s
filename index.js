1c0400993e4dec261cbcd0e90248648b5d3aa7fe
user.name: "Kashif Imran <user.email: "itz.real.kashif@gmail.com>
2024-11-08 17:48:03 +0500

    fixed route error

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/create-account', async (req, res) => {
     }
 })
 
-app.get('/get-user:id', async(req, res) => {
+app.get('/get-user/:id', async(req, res) => {
     try {
         const userId = req.params.id;
         if (!userId) {
